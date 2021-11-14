23e13efb60f6a84837ccd7ea0921f0a0bbfac238
Shine Joseph <shinejoseph@outlook.com>
2021-11-14 14:35:16 +0400

    first commit

---
+++ app/src/main/java/com/test/mvvmlist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.mvvmlist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.mvvmlist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
